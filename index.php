4ef28edc671386d8a2cb0b71ebbe57a1129f2146
Renato Nunes <r.silveira.nunes14@gmail.com>
2021-09-27 20:59:46 -0300

    index.php switch methods crud

--- index.php
+++ index.php
@@ -5,14 +5,28 @@ require 'class/Produto.php';
 $produto = new Produto();
 
 switch ($_GET['operacao']) {
+
+    case 'insert':
+        $status = $produto->insert('novo', 'produto teste');
+        echo ($status > 0) ? 'Produto cadastrado!' : 'Falha ao cadastrar!';
+        break;
+    
+    case 'update':
+        $status = $produto->update('teste', 1);
+        echo ($status > 0) ? 'Produto atualizado!' : 'Falha ao atualizar!';
+        break;
+    
+    case 'delete':
+        $status = $produto->delete(1);
+        echo ($status > 0) ? 'Produto excluído!' : 'Falha ao excluir!';
+        break;
+    
     case 'list':
+    default:
         echo '<h3>Produtos</h3>';
 
         foreach($produto->list() as $value){
             echo 'Id: '. $value['id'] . '<br>Nome: '. $value['nome'].'<br>Descrição: '. $value['descricao'].'<hr>';
         }
-        break;
-        
-    default:
         break;
 }
